ccc90d5bea77394c773115604e487843c647b4b1
Jakob Stigenberg <jakob@jakke.se>
2016-12-02 09:26:11 +0100

    Tiny

--- src/Jakkes.WebSockets/Class1.cs
+++ src/Jakkes.WebSockets/Class1.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Jakkes.WebSockets
 {
-    public class Class1
+    public class WebSocketServer
     {
-        public Class1()
+        private TcpListener _server;
+        public WebSocketServer(int port) : this(IPAddress.Any, port)
         {
+
         }
+        public WebSocketServer(IPAddress ip, int port)
+        {
+            _server = new TcpListener(ip, port);
+        }
+        public void Start() => _server.Start();
     }
 }
